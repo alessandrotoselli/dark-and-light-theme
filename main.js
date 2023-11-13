bff234ca941a85f3cd2e52f784ba2ff9fc375969
Alessandro Toselli <alessandrotoselli@MinidiAessandro.wind3.hub>
2023-11-13 22:57:12 +0100

    Adding index.html, main.js and style.css

---
+++ main.js
@@ -0,0 +1,30 @@
+const themeSwithBtn = document.getElementById('switch-btn');
+const currentTheme = document.documentElement;
+
+themeSwithBtn.textContent = "S";
+
+themeSwithBtn.addEventListener('click', () => {
+    if(themeSwithBtn.classList.contains('day-time')){
+        changeTheme('M');
+    } else {
+        changeTheme('S');
+    }
+})
+
+function changeTheme(theme) {
+    if (theme === 'M'){
+        currentTheme.style.setProperty('--theme-background', '#272643');
+        currentTheme.style.setProperty('--theme-text', '#e4eaff');
+        currentTheme.style.setProperty('--theme-box', '#2c698d');
+        themeSwithBtn.classList.remove('day-time');
+        themeSwithBtn.classList.add('night-time');
+        themeSwithBtn.textContent = "M";
+    } else if (theme === 'S'){
+        currentTheme.style.setProperty('--theme-background', '#e4eaff');
+        currentTheme.style.setProperty('--theme-text', '#000');
+        currentTheme.style.setProperty('--theme-box', '#bae8e8');
+        themeSwithBtn.classList.remove('night-time');
+        themeSwithBtn.classList.add('day-time');
+        themeSwithBtn.textContent = "S";
+    }
+}
