2ddd9be0f139b37e545a8a31327f44d0208276c2
Alessandro Toselli <alessandrotoselli@MinidiAessandro.wind3.hub>
2023-11-13 23:27:26 +0100

    New color palette and new toggle button

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const themeSwithBtn = document.getElementById('switch-btn');
 const currentTheme = document.documentElement;
 
-themeSwithBtn.textContent = "S";
+themeSwithBtn.innerHTML = "<i class='bx bx-sun' ></i>";
 
 themeSwithBtn.addEventListener('click', () => {
     if(themeSwithBtn.classList.contains('day-time')){
@@ -18,13 +18,13 @@ function changeTheme(theme) {
         currentTheme.style.setProperty('--theme-box', '#2c698d');
         themeSwithBtn.classList.remove('day-time');
         themeSwithBtn.classList.add('night-time');
-        themeSwithBtn.textContent = "M";
+        themeSwithBtn.innerHTML = "<i class='bx bx-moon' ></i>";
     } else if (theme === 'S'){
-        currentTheme.style.setProperty('--theme-background', '#e4eaff');
+        currentTheme.style.setProperty('--theme-background', '#ffffdf');
         currentTheme.style.setProperty('--theme-text', '#000');
         currentTheme.style.setProperty('--theme-box', '#bae8e8');
         themeSwithBtn.classList.remove('night-time');
         themeSwithBtn.classList.add('day-time');
-        themeSwithBtn.textContent = "S";
+        themeSwithBtn.innerHTML = "<i class='bx bx-sun' ></i>";
     }
 }
